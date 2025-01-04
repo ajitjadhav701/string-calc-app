a29dcd48f088a12f490fe264606ab91c1ab2a880
ajitjadhav701 <ajitjadhav701@gmail.com>
2025-01-04 21:25:50 +0530

    style: install tailwind css for styling

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,10 @@ import './App.css';
 
 function App() {
   return (
-    <div data-testid="app-component" className="App">
-      <header className="App-header">
-        <div>starter..</div>
-      </header>
+    <div data-testid="app-component" className="text-center">
+      <div className="flex flex-col items-center justify-center h-screen">
+        <div className='text-5xl'>starter..</div>
+      </div>
     </div>
   );
 }
